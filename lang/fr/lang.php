5dfacb2b4f602a3b126b190aac5825c3533ef54c
Damien Regad <dregad@mantisbt.org>
2018-04-18 10:40:34 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Damien Regad <dregad@mantisbt.org>
+ */
+$lang['user_list_use_wordpress'] = 'Cette liste est en lecture seule. Veuillez utiliser Wordpress pour gérer les utilisateurs.';
+$lang['error_filters_unsupported'] = 'Les filtres ne sont pas supportés';
+$lang['error_connect_failed']  = 'Impossible de se connecter à la base de données WordPress (%s)<br>Veuillez vérifier vos paramètres.';
