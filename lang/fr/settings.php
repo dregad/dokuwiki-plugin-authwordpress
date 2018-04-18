94c474e5a9645f4841bec002b499c174705e4273
Damien Regad <dregad@mantisbt.org>
2018-04-18 10:40:34 +0200

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Damien Regad <dregad@mantisbt.org>
+ */
+$lang['hostname']              = 'Nom du serveur hébergeant la base de données MySQL de WordPress';
+$lang['port']                  = 'Numéro de port utilisé par le serveur de base de données';
+$lang['database']              = 'Nom de la base de données WordPress';
+$lang['username']              = 'Nom de l\'utilisateur de la base de données';
+$lang['password']              = 'Mot de passe de l\'utilisateur de la base de données';
+$lang['prefix']                = 'Préfixe des tables de la base de données WordPress';
+$lang['usedefaultgroup']       = 'Si la case est cochée, l\'utilisateur deviendra membre du <a href="http://www.dokuwiki.org/config:defaultgroup">groupe par défaut</a>, en plus de celui correspondant à son rôle WordPress.';
