48e2dfaf04614683a407c18e85588272cd1fea11
Harriet Neitz <harrietneitz@gmail.com>
2018-04-24 13:05:39 +0200

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Harriet Neitz <harrietneitz@gmail.com>
+ */
+$lang['hostname']              = 'Naam van de server waarop de WordPress MySQL database draait';
+$lang['port']                  = 'Poortnummer waarop de database server luistert';
+$lang['database']              = 'WordPress database naam';
+$lang['username']              = 'Gebruikersnaam voor de database';
+$lang['password']              = 'Gebruikers\' wachtwoord voor de database';
+$lang['prefix']                = 'Voorvoegsel voor de WordPress Database Tabel';
+$lang['usedefaultgroup']       = 'Indien aangevinkt wordt de ingelogde gebruiker tevens lid van de <a href="http://www.dokuwiki.org/config:defaultgroup">default group</a>, en niet alleen van de groep die correspondeert met diens WordPress rol.';
