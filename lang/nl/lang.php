06009b90ff1b85cf97d8a08126ab411658c3cf5c
Harriet Neitz <harrietneitz@gmail.com>
2018-04-24 13:05:39 +0200

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Harriet Neitz <harrietneitz@gmail.com>
+ */
+$lang['user_list_use_wordpress'] = 'Deze lijst is Read Only. Maak gebruik van WordPress voor gebruikersbeheer.';
+$lang['error_filters_unsupported'] = 'Filters worden niet ondersteund.';
+$lang['error_connect_failed']  = 'Niet in staat te verbinden met WordPress database (%s)<br> Controleer je instellingen.';
