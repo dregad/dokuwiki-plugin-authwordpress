5053c4900d360ae9ee9c95625c956c72005585b4
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 15:45:39 +0200

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['hostname']              = 'Nome do servidor que hospeda o banco de dados MySQL do WordPress';
+$lang['port']                  = 'Número da porta em que o servidor de banco de dados está escutando';
+$lang['database']              = 'Nome do banco de dados WordPress';
+$lang['username']              = 'Nome do usuário do banco de dados';
+$lang['password']              = 'Senha do usuário do banco de dados';
+$lang['prefix']                = 'Prefixo de tabela do banco de dados WordPress';
+$lang['usedefaultgroup']       = 'Se marcado, o usuário logado se tornará membro do <a href="http://www.dokuwiki.org/config:defaultgroup"> grupo padrão </a>, além do grupo correspondente ao papel no Wordpress.';
