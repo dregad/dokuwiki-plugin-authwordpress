a1f86d476e3591d8dcf23ba886ce20a89e4b3844
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 15:45:39 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['user_list_use_wordpress'] = 'Esta lista é somente leitura. Por favor, use o Wordpress para gerenciar usuários.';
+$lang['error_filters_unsupported'] = 'Filtros não são suportados.';
+$lang['error_connect_failed']  = 'Não é possível conectar-se ao banco de dados do WordPress (%s) <br> Por favor, verifique suas configurações.';
