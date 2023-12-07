6aec856270980801abdef42fbaa81ecec6ab64b6
粗音未来 <miku39@onsei.fans>
2023-12-07 09:30:13 +0100

    translation update

---
+++ lang/zh/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 粗音未来 <miku39@onsei.fans>
+ */
+$lang['user_list_use_wordpress'] = '该列表为只读。请使用WordPress来管理用户。';
+$lang['error_filters_unsupported'] = '无法筛选。';
+$lang['error_connect_failed']  = '无法连接到WordPress数据库(%s)<br>请检查你的设置。';
---
+++ lang/zh/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 粗音未来 <miku39@onsei.fans>
+ */
+$lang['hostname']              = 'WordPress使用的MySQL数据库主机名';
+$lang['port']                  = '数据库所监听的端口';
+$lang['database']              = 'WordPress数据库名';
+$lang['username']              = '数据库用户名';
+$lang['password']              = '数据库密码';
+$lang['prefix']                = 'WordPress数据库表的前缀';
+$lang['usedefaultgroup']       = '如果打勾，登入的用户除了会保留其在WordPress的用户组之外，也会成为<a href="http://www.dokuwiki.org/config:defaultgroup">default group</a>的一员。';
