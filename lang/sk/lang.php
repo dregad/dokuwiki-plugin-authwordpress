346c05245ea83c3814d50db4ad072de0513bcad8
Wizzard <wizzardsk@gmail.com>
2026-06-04 21:35:57 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['user_list_use_wordpress'] = 'Tento zoznam je iba na čítanie. Na správu používateľov použite WordPress.';
+$lang['error_filters_unsupported'] = 'Filtre nie sú podporované.';
+$lang['error_connect_failed']  = 'Nepodarilo sa pripojiť k WordPress databáze (%s)<br>Skontrolujte prosím nastavenia.';
---
+++ lang/sk/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['hostname']              = 'Názov servera hosťujúceho WordPress MySQL databázu';
+$lang['port']                  = 'Číslo portu, na ktorom databázový server počúva';
+$lang['database']              = 'Názov WordPress databázy';
+$lang['username']              = 'Meno používateľa databázy';
+$lang['password']              = 'Heslo používateľa databázy';
+$lang['prefix']                = 'Prefix tabuliek WordPress databázy';
+$lang['usedefaultgroup']       = 'Ak je zaškrtnuté, prihlásený používateľ sa stane členom <a href="http://www.dokuwiki.org/config:defaultgroup">predvolenej skupiny</a>, popri skupine zodpovedajúcej jeho WordPress role.';
